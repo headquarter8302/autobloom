fca2a053e1a07b903e3db6f2bb176e6bd0144cdd
Markus <65797058+headquarter8302@users.noreply.github.com>
2023-07-22 05:31:36 -0400

    add resolution check

--- src/run.bat
+++ src/run.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 if not [%1]==[ch] (start conhost cmd /c "" "%~dpnx0" ch & exit)
 powershell -NoP -W hidden ; exit
 set "_home=%~dp0"
@@ -6,9 +7,14 @@ cd /D "%_home%"
 
 powershell -NoP .\wallpaper.ps1 "%_home%..\media\first.bmp"
 
+for /f "tokens=3 delims= " %%i in ('wmic path Win32_VideoController get CurrentHorizontalResolution^,CurrentVerticalResolution ^| findstr /r /c:"^[0-9]"') do (
+  set "MONITOR_WIDTH=%%i"
+  set "MONITOR_HEIGHT=%%j"
+)
+
 timeout 1
-start /B ..\mpv\mpv ..\media\bloom.mp4 --no-osc --no-input-default-bindings --no-taskbar-progress --no-window-dragging ^
-    --deband --hwdec=auto --no-border --cache=yes --demuxer-max-bytes=488281KiB --demuxer-readahead-secs=120 --autofit=100%% --player-operation-mode=pseudo-gui --input-ipc-server=\\.\pipe\mpvsocket --force-window=yes --volume=0
+start /B ..\mpv\mpv ..\media\bloom.mp4 --geometry=!MONITOR_WIDTH!x!MONITOR_HEIGHT!  --no-osc --no-input-default-bindings --no-taskbar-progress --no-window-dragging ^
+    --deband --hwdec=auto --no-border --cache=yes --demuxer-max-bytes=488281KiB --demuxer-readahead-secs=6 --autofit=100%% --player-operation-mode=pseudo-gui --input-ipc-server=\\.\pipe\mpvsocket --force-window=yes --volume=0
 cd ..\src
 timeout 2
 powershell -NoP .\wallpaper.ps1 "%_home%..\media\last.bmp"
